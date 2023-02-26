4f92d17a1f63ee18123850adc131ccf679d74381
IamEg <iameg@iameg.com>
2023-02-26 10:32:46 +0300

    next js init

---
+++ pages/index.tsx
@@ -0,0 +1,23 @@
+import Head from 'next/head'
+import { Inter } from 'next/font/google'
+import styles from '@/styles/Home.module.css'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Pokedex</title>
+      </Head>
+      <main className={styles.main}>
+        <div className={styles.description}>
+          <p>
+            Get started by editing&nbsp;
+            <code className={styles.code}>pages/index.tsx</code>
+          </p>
+        </div>
+      </main>
+    </>
+  )
+}
