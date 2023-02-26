e50c18493a5e7aaa2a491e8faf0e48da9367cc82
IamEg <iameg@iameg.com>
2023-02-26 13:24:59 +0300

    about page completed

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,7 +1,65 @@
-import React from 'react'
+import React from 'react';
+import {
+  Box,
+  VStack,
+  Button,
+  Flex,
+  Divider,
+  Text,
+  chakra,
+  Grid,
+  GridItem,
+  Container,
+  Stack,
+  useColorModeValue
+} from '@chakra-ui/react';
+import Link from 'next/link';
 
 export default function About() {
+
+  const textColor = useColorModeValue('gray.500','red.200');
   return (
-    <div>About</div>
-  )
+    <Box as={Container} maxW="7xl" mt={14} p={4}>
+      <Grid
+        templateColumns={{
+          base: "repeat(1, 1fr)",
+          sm: "repeat(2, 1fr)",
+          md: "repeat(2, 1fr)",
+        }}
+        gap={4}
+      >
+        <GridItem colSpan={1}>
+          <VStack alignItems="flex-start" spacing="20px">
+            <chakra.h2 fontSize="3xl" fontWeight="700">
+              About
+            </chakra.h2>
+            <Link href="/">
+              <Button colorScheme="red" size="md">
+                To Homepage
+              </Button>
+            </Link>
+          </VStack>
+        </GridItem>
+        <GridItem>
+          <Stack spacing={3}>
+            <Flex>
+              <Text as="i" color={textColor}>
+                This is a simple pokedex implemented with Next.js. The Sole
+                purpose of this project is to learn Next js and nothing more.
+                As for the styling, Chakra UI has been used for providing
+                default template.
+              </Text>
+            </Flex>
+            <Flex>
+              <Text as="i" color={textColor}>
+                The layout of the page consists of Navbar, Sidebar, Children
+                Content and Footer. The Page have been determined to be
+                implemented as simplistic as possible.
+              </Text>
+            </Flex>
+          </Stack>
+        </GridItem>
+      </Grid>
+    </Box>
+  );
 }
